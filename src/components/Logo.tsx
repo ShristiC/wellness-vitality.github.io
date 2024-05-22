8987d182b357b96bc82b6637e791f32a3a9ef704
Sanjit Bhat <sanjit.bhat@gmail.com>
2024-05-22 14:11:34 -0400

    properly import img rsrcs

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,15 @@
+import LogoImage from "../assets/logo_cover_transparent.png";
+import LogoIconImage from "../assets/logo_icon_transparent.png";
+
 function Logo() {
     return (
-        <img src='../src/assets/logo_cover_transparent.png' alt='A tree whose roots are shaped out of a spiral, and the leaves are getting larger in size as they emerge outside the circle. Next to it, the words Wellnes n Vitality.' width={250} />
+        <img src={LogoImage} alt='A tree whose roots are shaped out of a spiral, and the leaves are getting larger in size as they emerge outside the circle. Next to it, the words Wellnes n Vitality.' width={250} />
     );
 }
 
 export function LogoIcon() {
     return (
-        <img src='../src/assets/logo_icon_transparent.png' alt='A tree whose roots are shaped out of a spiral, and the leaves are getting larger in size as they emerge outside the circle.' width={100} />
+        <img src={LogoIconImage} alt='A tree whose roots are shaped out of a spiral, and the leaves are getting larger in size as they emerge outside the circle.' width={100} />
     );
 }
 
