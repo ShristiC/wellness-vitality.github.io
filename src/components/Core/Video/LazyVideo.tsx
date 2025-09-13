40f2bed270b4f4d9c1f5c99f7f83c4e33b61054d
Shristi Chitlangia <shristichitlangia2001@gmail.com>
2025-09-13 16:51:25 -0400

    feature/updated testimonials and thumbnails

--- src/components/Core/Video/LazyVideo.tsx
+++ src/components/Core/Video/LazyVideo.tsx
@@ -1,7 +1,14 @@
 import { useState, useEffect } from "react";
 
-export default function LazyVideo({ videoUrl, title }: { videoUrl: string; title: string }) {
+interface ILazyVideoProps {
+    videoId: string;
+    title: string;
+}
+
+export default function LazyVideo({ videoId, title }: ILazyVideoProps) {
     const [isLoaded, setIsLoaded] = useState(false);
+    const videoUrl = `https://www.youtube.com/embed/${videoId}?modestbranding=1&rel=0&showinfo=0&controls=1&iv_load_policy=3&fs=0 `;
+    const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
 
     useEffect(() => {
         const observer = new IntersectionObserver(
@@ -22,11 +29,22 @@ export default function LazyVideo({ videoUrl, title }: { videoUrl: string; title
     return isLoaded ? (
         <iframe
             src={videoUrl}
-            title={title}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
         />
     ) : (
-        <div style={{ width: "100%", height: "200px", backgroundColor: "#ccc" }}>Loading...</div>
+        <div
+            id={title}
+            style={{
+                width: "100%",
+                height: "200px",
+                backgroundColor: "#000",
+                backgroundImage: `url(${thumbnailUrl})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                cursor: "pointer",
+            }}
+            onClick={() => setIsLoaded(true)} // Load the video when the thumbnail is clicked
+        />
     );
 }
--- src/components/Pages/Testimonials.tsx
+++ src/components/Pages/Testimonials.tsx
@@ -20,23 +20,31 @@ export default function TestimonialsPage() {
             description: "Meet Cameron, who had acute constipation for 2 decades, acid reflux, knee pain, and brain fog. In a month, all of it has been reversed.",
             thumbnailTitle: "Cameron's Journey",
             tag: "Constipation, Acid Reflux & Knee Pain",
-            videoUrl: "https://www.youtube.com/embed/YzLEwxfVGQ4?si=rD0atdX03HAm2dDi",
+            videoId: "YzLEwxfVGQ4?si=rD0atdX03HAm2dDi",
             date: "May 2025"
         },
+        // {
+        //     title: "I am Not Internalizing the Saddness in the World. I now have a Brighter Outlook in Life",
+        //     description: "Meet Suzan Dimon, who stopped taking allergy shots, reduced steroid sprays, stopped taking cholestorl medication, eliminated muscle scramps, and lost 15 pounds.",
+        //     thumbnailTitle: "Suzan's Journey",
+        //     tag: "Allergies, Cholesterol & Weight Loss",
+        //     videoId: "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FWellnessnVitality%2Fvideos%2F3983651408538457%2F&show_text=false&width=476&t=0",
+        //     date: "April 2025"
+        // }, // need youtube
         {
-            title: "I am Not Internalizing the Saddness in the World. I now have a Brighter Outlook in Life",
-            description: "Meet Suzan Dimon, who stopped taking allergy shots, reduced steroid sprays, stopped taking cholestorl medication, eliminated muscle scramps, and lost 15 pounds.",
-            thumbnailTitle: "Suzan's Journey",
-            tag: "Allergies, Cholesterol & Weight Loss",
-            videoUrl: "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FWellnessnVitality%2Fvideos%2F3983651408538457%2F&show_text=false&width=476&t=0",
-            date: "April 2025"
+            title: "I Reveresed my Type II Diabetes",
+            description: "Meet Sonal, who reversed her Type II Diabetes within weeks.",
+            thumbnailTitle: "Sonal's Story",
+            tag: "Type II Diabetes",
+            videoId: "kPhsQI_eBRk?si=cTB68ym-1AkWnmWM",
+            date: "January 2025"
         },
         {
             title: "I lost 15lbs, and my Cholesterol Levels Reduced Significantly",
             description: "Meet Amit, who learned how to manage his lifestyle and food, leading to life changes that felt so difficult before.",
             thumbnailTitle: "Amit's Journey",
             tag: "Cholesterol & Weight Loss",
-            videoUrl: "https://youtube.com/embed/52I-bTy2mMU?si=53sFFiZby-dZ1WFm",
+            videoId: "52I-bTy2mMU?si=53sFFiZby-dZ1WFm",
             date: "June 2024"
         },
         {
@@ -44,40 +52,72 @@ export default function TestimonialsPage() {
             description: "Meet Satish, who reflects on his 40 day journey with Wellness & Vitality and achieves unimagined success in reducing Insulin Medication with Type II Diabetes.",
             thumbnailTitle: "Satish's Story",
             tag: "Type II Diabetes & Weight Loss",
-            videoUrl: "https://www.youtube.com/embed/j6GY5FYBKlk",
+            videoId: "j6GY5FYBKlk",
             date: "August 2023"
         },
         {
-            title: "I Reveresed my Type II Diabetes",
-            description: "Meet Sonal, who reversed her Type II Diabetes within weeks.",
-            thumbnailTitle: "Sonal's Story",
+            title: "Managed my Type II Diabetes without Medication",
+            description: "Meet Indira, who is 72 years old and has managed her Type II Diabetes without medication, and is feeling more energetic.",
+            thumbnailTitle: "Indira's Story",
             tag: "Type II Diabetes",
-            videoUrl: "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FWellnessnVitality%2Fvideos%2F309632591752273%2F&show_text=false&width=476&t=0",
-            date: "January 2025"
+            videoId: "kPhsQI_eBRk?si=Qa13pl7plCIllo-s",
+            date: "August 2023"
         },
         {
             title: "15 days - My Migrane Pain Went Away",
             description: "For 18 years, Shyam was struggling with severe migrane pains. Within 15 days, his migrane is gone and his sugar levels went down by 132 points.",
             thumbnailTitle: "Shyam's Journey",
             tag: "Migrane and Type II Diabetes",
-            videoUrl: "https://www.youtube.com/embed/E6h3_ut6bEg",
+            videoId: "E6h3_ut6bEg",
             date: "April 2023"
+        },
+        {
+            title: "3 months - I Reversed my Type II Diabetes",
+            description: "Meet Rahul, who reversed his Type II Diabetes, lost 11lbs, and is feeling healthier overall.",
+            thumbnailTitle: "Rahul's Journey",
+            tag: "Type II Diabetes & Weight Loss",
+            videoId: "-9tIFR7MiGs?si=iM1gfftC7adGd3Ap",
+            date: "Jan 2023"
         },
         {
             title: "How Wellness & Vitality helped my Arthritis",
             description: "Meet Catherine, who regained her life back from Severe Pain due to Arthritis and High Inflammation.",
             thumbnailTitle: "Catherine's Journey",
             tag: "Arthritis & Inflammation",
-            videoUrl: "https://www.youtube.com/embed/iS0Z10eWd9o",
+            videoId: "iS0Z10eWd9o",
             date: "August 2022"
+        },
+        {
+            title: "I Reversed My Type II Diabetes",
+            description: "Meet Vikas, who reversed his Type II Diabetes and is off all his medications.",
+            thumbnailTitle: "Vikas's Journey",
+            tag: "Type II Diabetes",
+            videoId: "2VNg5YESa9o?si=SMXQrP7VO3pSHNSH",
+            date: "April 2022"
         },
         {
             title: "How Wellness & Vitality regained my Self-Confidence",
             description: "Meet Hasita, who struggled with her self-confidence due to obesity and weight. She meets her goals in sustainable ways and is on the journey to regain her life back",
             thumbnailTitle: "Hasita's Journey",
             tag: "Weight Loss",
-            videoUrl: "https://www.youtube.com/embed/ZwkWNf4cI_c",
+            videoId: "ZwkWNf4cI_c",
             date: "March 2022"
+        },
+        {
+            title: "I regained my Stamina and My Rashes were Reduced by 50%",
+            description: "Meet Shefali, who regained her stamina for exercising and was finally able to address skin rashes on her face and hands.",
+            thumbnailTitle: "Shefali's Journey",
+            tag: "Skin Rashes & Stamina",
+            videoId: "T_-S6ZJo-cs?si=SER8iqRAhdYpqacR",
+            date: "Nov 2021"
+        },
+        {
+            title: "In 90 Days - I Reduced Medication by 75%",
+            description: "Meet Radhe. He was suffering from diabetes since long and in 3 months turned his life around so drastically that his doctor was also shocked!",
+            thumbnailTitle: "Radhe's Journey",
+            tag: "Type II Diabetes",
+            videoId: "BlqDOuy4M5s?si=IZxXL_r7z-5ahe15",
+            date: "Nov 2021"
         },
     ];
 
@@ -90,11 +130,16 @@ export default function TestimonialsPage() {
             <TestimonialsCoverComponent />
             <VideoTestimonials $isMobile={isMobile}>
                 <CoverVideoWrapper $isMobile={isMobile}>
-                    <Video $isMobile={isMobile} src={testimonialVideos[currIndex].videoUrl} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title={testimonialVideos[currIndex].title}/>
-                    <CoverVideoContent $isMobile={isMobile}>
-                        <HeadingText $isMobile={isMobile} $color="primary">{testimonialVideos[currIndex].title}</HeadingText>
-                        <ContentText $isMobile={isMobile}>{testimonialVideos[currIndex].description}</ContentText>
-                    </CoverVideoContent>
+                <Video
+                    $isMobile={isMobile}
+                    src={`https://www.youtube.com/embed/${testimonialVideos[currIndex].videoId}`} // Construct the embed URL
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                />
+                <CoverVideoContent $isMobile={isMobile}>
+                    <HeadingText $isMobile={isMobile} $color="primary">{testimonialVideos[currIndex].title}</HeadingText>
+                    <ContentText $isMobile={isMobile}>{testimonialVideos[currIndex].description}</ContentText>
+                </CoverVideoContent>
                 </CoverVideoWrapper>
                 <ContentTextBold $isMobile={isMobile}>See More</ContentTextBold>
                 <TestimonialsRow>
@@ -105,7 +150,7 @@ export default function TestimonialsPage() {
                                 title={testimonial.thumbnailTitle} 
                                 tag={testimonial.tag} 
                                 date={testimonial.date} 
-                                videoURL={testimonial.videoUrl}
+                                videoId={testimonial.videoId}
                                 onClick={() => setTestimonial(i)}/>
                         );
                     })}
@@ -121,15 +166,15 @@ interface TestimonialCardProps {
     title: string;
     tag: string;
     date: string;
-    videoURL: string;
+    videoId: string;
     onClick: () => void;
 }
 
-function TestimonialCard ({title, date, videoURL, onClick}: TestimonialCardProps) {
+function TestimonialCard ({title, date, videoId, onClick}: TestimonialCardProps) {
     const [_, isMobile] = useWindowDimensions();
     return (
         <Card role="button" onClick={onClick}>
-            <LazyVideo videoUrl={videoURL} title={title} />
+            <LazyVideo videoId={videoId} title={title} />
             <CardContent>
                 <ContentText $isMobile={isMobile}>{title}</ContentText>
                 <Text $isMobile={isMobile}>{date}</Text>
@@ -170,7 +215,7 @@ const Card = styled(Row)`
     background-color: rgba(128, 128, 128, 0.2); // gray with 20% opacity
     border-radius: ${BorderRadius.button}px;
     padding: 15px;
-    min-width: 300px;
+    min-width: 500px;
     gap: 15px;
     justify-content: space-evenly;
     height: 150px;
