54ff83cdf3fec37360f9786053042e3fb8ee0bb6
Shristi Chitlangia <shristichitlangia2001@gmail.com>
2026-09-20 18:00:40 -0500

    linking image

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -42,6 +42,10 @@ export default function Seo({
       <meta property="og:description" content={description} />
       <meta property="og:url" content={canonical} />
       <meta property="og:image" content={image} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:type" content="image/jpeg" />
+      <meta property="og:image:alt" content={`${SITE_NAME} logo`} />
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
